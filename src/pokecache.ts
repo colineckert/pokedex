f553d0e9262aa08478d28c42c34e10a8df108f1a
Colin Eckert <colineckert10@gmail.com>
2025-07-30 08:28:42 -0700

    init pokecache

---
+++ src/pokecache.ts
@@ -0,0 +1,48 @@
+export type CacheEntry<T> = {
+  createdAt: number;
+  val: T;
+};
+
+export class Cache {
+  #cache = new Map<string, CacheEntry<any>>();
+  #reapIntervalId: NodeJS.Timeout | undefined = undefined;
+  #interval: number;
+
+  constructor(interval: number = 60000) {
+    // Default to 1 minute
+    this.#interval = interval;
+    this.#startReapLoop();
+  }
+
+  add<T>(key: string, val: T) {
+    this.#cache.set(key, { createdAt: Date.now(), val });
+  }
+
+  get<T>(key: string): CacheEntry<T> | undefined {
+    return this.#cache.get(key) as CacheEntry<T> | undefined;
+  }
+
+  clear() {
+    this.#cache.clear();
+  }
+
+  #reap() {
+    const now = Date.now();
+    for (const [key, entry] of this.#cache.entries()) {
+      if (now - entry.createdAt > this.#interval) {
+        this.#cache.delete(key);
+      }
+    }
+  }
+
+  #startReapLoop() {
+    this.#reapIntervalId = setInterval(() => this.#reap(), this.#interval);
+  }
+
+  stopReapLoop() {
+    if (this.#reapIntervalId) {
+      clearInterval(this.#reapIntervalId);
+      this.#reapIntervalId = undefined;
+    }
+  }
+}
